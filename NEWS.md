298ba30ccd568ebea09258fac130ebb361d2adc9
stefan.b.fleck@gmail.com <stefan.fleck@statistik.gv.at>
2017-07-20 08:49:21 +0200

    Removed RCPP dependecies

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,11 @@
-# isit 0.0.1.9000
 
-* inition version
+# isit 0.0.2.9000
+
+* Removed RCPP dependency for better compatibility
+
+
+# isit 0.0.1
+
+* initial version
 * added is_sorted, is_ascending and is_descending
 * migrated all predicates from hammr
--- R/is_ascending.R
+++ R/is_ascending.R
@@ -11,12 +11,6 @@
 #' `is_ascending()` and `is_descending()` are generics, so you can easily 
 #' implement your own methods if you want to.
 #'  
-#' The methods for numeric and integer vectors are implemented
-#' in C++. `is_ascending()` has about the same performance as
-#' `!base::is.unsorted()`, but `is_descending(x)`is slighlty faster 
-#' than `!base::is.unsorted(-x)` as it does not need to reverse / negate the
-#' vector first. 
-#' 
 #' The default methods fall back to wrapping [base::is.unsorted()], and 
 #' therefore support all the same classes (character, complex, raw and others)
 #' at the same performance.
@@ -43,38 +37,6 @@ is_ascending.default <- function(x, na.rm = FALSE){
 
 
 
-#' @export
-is_ascending.integer <- function(x, na.rm = FALSE) {
-  if (anyNA(x)) {
-    if (!na.rm) {
-      return(NA)
-    } else {
-      x <- x[!is.na(x)]
-    }
-  } 
-  
-  .Call('_isit_is_ascending_integer', PACKAGE = 'isit', x)
-}
-
-
-
-
-#' @export
-is_ascending.numeric <- function(x, na.rm = FALSE) {
-  if (anyNA(x)) {
-    if (!na.rm) {
-      return(NA)
-    } else {
-      x <- x[!is.na(x)]
-    }
-  } 
-  
-  .Call('_isit_is_ascending_numeric', PACKAGE = 'isit', x)
-}
-
-
-
-
 #' @rdname is_ascending
 #' @export
 is_strictly_ascending <- function(x, na.rm = FALSE){
@@ -87,38 +49,6 @@ is_strictly_ascending <- function(x, na.rm = FALSE){
 #' @export
 is_strictly_ascending.default <- function(x, na.rm = FALSE){
   is.unsorted(x, na.rm = na.rm, strictly = TRUE)
-}
-
-
-
-
-#' @export
-is_strictly_ascending.integer <- function(x, na.rm = FALSE) {
-  if (anyNA(x)) {
-    if (!na.rm) {
-      return(NA)
-    } else {
-      x <- x[!is.na(x)]
-    }
-  } 
-  
-  .Call('_isit_is_strictly_ascending_integer', PACKAGE = 'isit', x)
-}
-
-
-
-
-#' @export
-is_strictly_ascending.numeric <- function(x, na.rm = FALSE) {
-  if (anyNA(x)) {
-    if (!na.rm) {
-      return(NA)
-    } else {
-      x <- x[!is.na(x)]
-    }
-  } 
-  
-  .Call('_isit_is_strictly_ascending_numeric', PACKAGE = 'isit', x)
 }
 
 
@@ -143,38 +73,6 @@ is_descending.default <- function(x, na.rm = FALSE){
 
 
 
-#' @export
-is_descending.integer <- function(x, na.rm = FALSE) {
-  if (anyNA(x)) {
-    if (!na.rm) {
-      return(NA)
-    } else {
-      x <- x[!is.na(x)]
-    }
-  } 
-  
-  .Call('_isit_is_descending_integer', PACKAGE = 'isit', x)
-}
-
-
-
-
-#' @export
-is_descending.numeric <- function(x, na.rm = FALSE) {
-  if (anyNA(x)) {
-    if (!na.rm) {
-      return(NA)
-    } else {
-      x <- x[!is.na(x)]
-    }
-  } 
-  
-  .Call('_isit_is_descending_numeric', PACKAGE = 'isit', x)
-}
-
-
-
-
 #' @rdname is_ascending
 #' @export
 is_strictly_descending <- function(x, na.rm = FALSE){
@@ -187,38 +85,6 @@ is_strictly_descending <- function(x, na.rm = FALSE){
 #' @export
 is_strictly_descending.default <- function(x, na.rm = FALSE){
   is.unsorted(rev(x), na.rm = na.rm, strictly = TRUE)
-}
-
-
-
-
-#' @export
-is_strictly_descending.integer <- function(x, na.rm = FALSE) {
-  if (anyNA(x)) {
-    if (!na.rm) {
-      return(NA)
-    } else {
-      x <- x[!is.na(x)]
-    }
-  } 
-  
-  .Call('_isit_is_strictly_descending_integer', PACKAGE = 'isit', x)
-}
-
-
-
-
-#' @export
-is_strictly_descending.numeric <- function(x, na.rm = FALSE) {
-  if (anyNA(x)) {
-    if (!na.rm) {
-      return(NA)
-    } else {
-      x <- x[!is.na(x)]
-    }
-  } 
-  
-  .Call('_isit_is_strictly_descending_numeric', PACKAGE = 'isit', x)
 }
 
 
